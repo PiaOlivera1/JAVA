81f2b9f8d4b3355f55e02203e94c28a72b117f5d
PiaOlivera1 <piaoliveraa@gmail.com>
2025-10-21 10:18:49 -0300

    funciones

--- IntroduccionJava/src/array.java
+++ IntroduccionJava/src/array.java
@@ -13,27 +13,49 @@ public class array {
         listaProducto.add("Pera");
         listaProducto.add("Kiwi");
 
-        System.out.println("Frutas disponibles: ");
-        for (String producto : listaProducto){
+        listarProductosporfuncion(listaProducto);
+        productosEnMayuscula(listaProducto);
+//        System.out.println("Frutas disponibles: ");
+//        for (String producto : listaProducto){
+//            System.out.println(producto);
+//        }
+
+//        listaProducto.remove("Pera");
+//        listaProducto.add("Manzana ");
+//        listaProducto.add("Manzana verde");
+//        System.out.println("Frutas modificadas: ");
+//        for (String producto : listaProducto){
+//            System.out.println(producto);
+//        }
+//
+//        //Buscar producto
+//        System.out.println("Ingrese la categoria que quiere ver: ");
+//        var verduleria = entrada.nextLine();
+//        ///System.out.println("Productos con la letra M");
+//        for (String producto : listaProducto){
+//            if(producto.charAt(0)== 'M'){
+//                System.out.println(producto);
+//            }
+//        }
+     }
+
+    public static void listarProductosporfuncion(ArrayList<String> productos)
+    {
+        System.out.println("***** Listar productos: ******");
+
+        for (var producto :productos){
             System.out.println(producto);
         }
 
-        listaProducto.remove("Pera");
-        listaProducto.add("Manzana ");
-        listaProducto.add("Manzana verde");
-        System.out.println("Frutas modificadas: ");
-        for (String producto : listaProducto){
-            System.out.println(producto);
-        }
+    }
 
-        //Buscar producto
-        System.out.println("Ingrese la categoria que quiere ver: ");
-        var verduleria = entrada.nextLine();
-        ///System.out.println("Productos con la letra M");
-        for (String producto : listaProducto){
-            if(producto.charAt(0)== 'M'){
-                System.out.println(producto);
-            }
+    public static void productosEnMayuscula(ArrayList<String>productos){
+        System.out.println("Mostramos los productos en mayúscula:");
+
+        for (var producto : productos){
+            producto = producto.trim().toUpperCase();
+            System.out.println(producto);
         }
     }
+
 }
