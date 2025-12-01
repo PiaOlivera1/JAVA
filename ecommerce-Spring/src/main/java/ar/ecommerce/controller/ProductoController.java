57a54d79063daee664fe382391ad18d59c1940d9
PiaOlivera1 <piaoliveraa@gmail.com>
2025-12-01 16:40:49 -0300

    avance e-commerce
    implementamos el controller
    agregamos la clase de carrito y carrItem para empezar a incorporar el carrito de comprar.

---
+++ ecommerce-Spring/src/main/java/ar/ecommerce/controller/ProductoController.java
@@ -0,0 +1,21 @@
+//package ar.ecommerce.controller;
+//
+//import ar.ecommerce.modelo.Producto;
+//import ar.ecommerce.servicio.IProductoServicio;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.*;
+//import java.util.List;
+//
+
+//public class ProductoController {
+//
+//    @GetMapping("")
+//    public String show(){
+//        return  "productos/show";
+//    }
+
+
+
+
+}
